03a42c669bc52937d29c6df6e547c7f0e07d8161
Mani-Bot-Cypher <manishankarch6805@gmail.com>
2025-04-16 14:24:41 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    MYSQL_HOST = 'leave-db'  # Name of the MySQL service in OpenShift
+    MYSQL_HOST = 'mysql'  # Name of the MySQL service in OpenShift
     MYSQL_USER = 'leaveuser'  # from Secret
     MYSQL_PASSWORD = 'leavepass123'  # from Secret
     MYSQL_DATABASE = 'leavedb'  # from Secret
